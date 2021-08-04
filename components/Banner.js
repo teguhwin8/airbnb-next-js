448601fd6c55d8d6b1d603f17b3d3183ff63b9a2
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2021-08-05 00:03:12 +0700

    Header and Banner

---
+++ components/Banner.js
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+const Banner = () => {
+	return (
+		<div className="relative h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+			<Image
+				src="https://links.papareact.com/0fm"
+				layout="fill"
+				objectFit="cover"
+			/>
+		</div>
+	)
+}
+
+export default Banner
