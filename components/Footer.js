7bfe97b23c476b532acb87e176747e40b3a02c4f
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2021-08-05 23:37:38 +0700

    Footer

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-16 md:px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
