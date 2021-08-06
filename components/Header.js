632524d284862d0a978331f1cb2ddb27f15172cb
teguhwin8 <43848500+teguhwin8@users.noreply.github.com>
2021-08-06 22:13:58 +0700

    react-date-range init

--- components/Header.js
+++ components/Header.js
@@ -6,9 +6,25 @@ import {
 	UserCircleIcon,
 } from "@heroicons/react/solid"
 import { useState } from "react"
+import "react-date-range/dist/styles.css" // main style file
+import "react-date-range/dist/theme/default.css" // theme css file
+import { DateRangePicker } from "react-date-range"
 
 function Header() {
 	const [searchInput, setSeacrhInput] = useState("")
+	const [startDate, setStartDate] = useState(new Date())
+	const [endDate, setEndDate] = useState(new Date())
+
+	const handleSelect = (ranges) => {
+		setStartDate(ranges.selection.startDate)
+		setEndDate(ranges.selection.endDate)
+	}
+
+	const selectionRange = {
+		startDate: startDate,
+		endDate: endDate,
+		key: "selection",
+	}	
 
 	return (
 		<header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
@@ -42,8 +58,13 @@ function Header() {
 			</div>
 
 			{searchInput && (
-				<div>
-					
+				<div className="flex flex-col col-span-3 mx-auto">
+					<DateRangePicker
+						ranges={[selectionRange]}
+						minDate={new Date()}
+						rangeColors={["#FD5B61"]}
+						onChange={handleSelect}
+					/>
 				</div>
 			)}
 		</header>
